2e9568dcd2597825ea4b5fb77dad2103f7d97d5d
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-11-24 23:50:42 +0600

    added serializers

---
+++ movies/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from .models import *
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class CinemaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cinema
+        fields = '__all__'
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+
+class ShowtimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Showtime
+        fields = '__all__'
+
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = '__all__'
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = '__all__'
+
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = '__all__'
+
+
+class PurchaseHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseHistory
+        fields = '__all__'
